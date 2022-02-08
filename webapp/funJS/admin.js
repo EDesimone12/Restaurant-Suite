0b8ffd3ab31960e8dc614cadc6774bcebe459927
eugenio12simone <eugenio12simone@gmail.com>
2022-02-08 19:20:15 +0100

    Update admin.js

---
+++ webapp/funJS/admin.js
@@ -0,0 +1,12 @@
+function deleteElem(event){
+    var inviare = event.target.getAttribute("value");
+
+    var xmlHttpReq = new XMLHttpRequest();
+    if (this.readyState == 4 && this.status == 200) {
+        inviare.parentElement.removeChild(inviare);
+
+    }
+
+    xmlHttpReq.open("POST", "ServletDeleteAj?codPrenotazione="+encodeURIComponent(inviare) , true);
+    xmlHttpReq.send();
+}
